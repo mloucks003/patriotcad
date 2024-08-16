d1dc49163c35a370173c1fda0d713400e8c24fb9
Michael Loucks <loucksmichael1234@gmail.com>
2024-08-16 07:15:42 -0600

    fixed dashboard html and css

--- patriot_cad/settings.py
+++ patriot_cad/settings.py
@@ -23,11 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p*$%zr6)z!*)y_&b1ji29k85m-6@j9*li0&%n8wqd2a21($gk('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['test.patriotcad.com', '129.153.176.32', '158.101.112.40', 'localhost']
+ALLOWED_HOSTS = ['test.patriotcad.com', '129.153.176.32', '158.101.112.40', 'localhost', '127.0.0.1']
 
-SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
+SESSION_EXPIRE_AT_BROWSER_CLOSE = FALSE
 
 
 # Application definition
