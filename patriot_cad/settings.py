42053b8df5fb741e3efc7cb56ef1a40a2f3429a5
mloucks003 <68255731+mloucks003@users.noreply.github.com>
2024-11-25 18:08:26 -0600

    add CORS

--- patriot_cad/settings.py
+++ patriot_cad/settings.py
@@ -42,9 +42,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_filters',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', 
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
